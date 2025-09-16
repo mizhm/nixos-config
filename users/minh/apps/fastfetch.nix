b3be942270714690f6a8c3f8f2d9565013845d1d
mizhm <minhnt141204@gmail.com>
2025-09-16 23:20:24 +0700

    feat: change theme to rose-pine

---
+++ users/minh/apps/fastfetch.nix
@@ -0,0 +1,3 @@
+{...}: {
+  xdg.configFile."fastfetch".source = ../dotfiles/fastfetch;
+}
---
+++ users/minh/dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "rebelot/kanagawa.nvim" },
+  { "vague2k/vague.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vague",
+    },
+  },
+}
