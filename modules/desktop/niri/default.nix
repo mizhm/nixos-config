6b6468ae3fc241ce0d1f1ebc50ae4537d170ec2b
mizhm <minhnt141204@gmail.com>
2025-09-23 00:21:26 +0700

    fix screensharing

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -1,10 +1,26 @@
 {pkgs, ...}: {
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   wayland.enable = true;
-  # };
-
   programs.niri.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+    config = {
+      common = {
+        default = ["gtk"];
+      };
+      niri = {
+        default = [
+          "gtk"
+          "gnome"
+        ];
+        "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
+        "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
+      };
+    };
+  };
 
   environment = {
     systemPackages = with pkgs; [
@@ -16,22 +32,24 @@
       swaynotificationcenter
       swaybg
       grim
-      xdg-desktop-portal-gtk
+      gnome-keyring
+      nautilus
       xwayland-satellite
     ];
 
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
+      XDG_CURRENT_DESKTOP = "niri";
+      XDG_SESSION_DESKTOP = "niri";
       ANKI_WAYLAND = "1";
       XDG_SESSION_TYPE = "wayland";
       MOZ_ENABLE_WAYLAND = "1";
       ELECTRON_OZONE_PLATFORM_HINT = "auto";
       CLUTTER_BACKEND = "wayland";
-      DISPLAY = null;
-      GDK_BACKEND = "wayland,x11";
       QT_QPA_PLATFORM = "wayland;xcb";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       SDL_VIDEODRIVER = "wayland";
+      GTK_USE_PORTAL = 1;
     };
   };
 }
